e3cf10c94dffd4543014120746c58c2b0e926d7e
King Penson II <KingMelo@users.noreply.github.com>
2022-09-16 08:08:51 -0500

    Update xsoarBlob.py

--- xsoarBlob.py
+++ xsoarBlob.py
@@ -1,7 +1,7 @@
 import requests
 
 #Provided when you generate a SAS Token in Azure
-blobUrl = "https://hobgoblin.blob.core.windows.net/edl/edllist.txt?sp=racwd&st=2022-09-15T17:39:38Z&se=2023-09-16T01:39:38Z&spr=https&sv=2021-06-08&sr=b&sig=zKc2ITY7hDAFRTj09cYLaWXCphgkE%2FeM0FYbICRLCj8%3D"
+blobUrl = ""
 
 #Prompt user for ip address
 ipAddress = input('Enter IP that you would like to block here: ')
